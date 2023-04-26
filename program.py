0479c84bcbcc7da7229544acb0c451deb083ef15
maratdmitrichenki <maratdmitrichenki@gmail.com>
2023-04-26 13:37:34 +0300

    add if array

--- program.py
+++ program.py
@@ -2,4 +2,5 @@ def b_sorter(array):
     length = len(array)
     for i in range(length):
         for j in range(0, length-i-1):
-            pass
+            if array[j] > array[j+1]:
+                pass
