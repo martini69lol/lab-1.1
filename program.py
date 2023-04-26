baa518bf4e81322c4a8f503055f670c3e3b1e94f
maratdmitrichenki <maratdmitrichenki@gmail.com>
2023-04-26 13:39:26 +0300

    add test

--- program.py
+++ program.py
@@ -5,3 +5,8 @@ def b_sorter(array):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
     return array
+
+
+array_for_sort = [4, 1, 3, 2, 6, 5, 7]
+
+print(b_sorter(array=array_for_sort))
